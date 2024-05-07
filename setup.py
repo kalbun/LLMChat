200d7aa04bb873a982c9809400b0303902830dc5
Giuseppe Scarpi <giuseppe.scarpi@tiscali.it>
2024-05-07 20:04:14 +0200

    [NEW] Module turned into a library by adding setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='LLMChat',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple python class that allows to easily execute completion tasks on GPT, Claude and Gemini',
+    author='Giuseppe Scarpi',
+    author_email='giuseppe.scarpi@unica.it',
+    keywords={'LLM', 'GPT'},
+)
