95a5d42c8961abb8e634270bf35b72950dc1c9aa
Giuseppe Scarpi <giuseppe.scarpi@tiscali.it>
2024-12-30 12:11:25 +0100

    [UPD] Unit tests modified to reflect changes in LLMCompletion() method. [UPD] Tests improved to check both one-shot and zero-shots behavrious.

--- LLMChatUnitTest.py
+++ LLMChatUnitTest.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import MagicMock
 from typing import List
 
 # Assuming the LLMChat.py is imported and available as LLMChat
@@ -7,6 +7,9 @@ from LLMChat import LLM, Models
 
 class TestLLM(unittest.TestCase):
 
+    testOneShot1: str = "My name is Tom"
+    testOneShot2: str = "What is my name? Answer one single word with no punctuation"
+    testOneShotAnswer: str = "Tom"
     testRole: str = "From now, echo back exactly what I write"
     testMessage: str = "*A* this is a test *B*"
 
@@ -38,13 +41,21 @@ class TestLLM(unittest.TestCase):
         answer: str = ""
         llm: LLM = LLM(Models.GPT)
         llm.DefineRole(self.testRole)
-        # new conversation appended to queue
-        answer = llm.LLMCompletion(self.testMessage,queue,True)
-        self.assertEqual(self.testMessage, answer)
-        self.assertEqual(len(queue),3)
+
+        # one-shot conversation with queue
+        llm.LLMCompletion(self.testOneShot1,queue)
+        answer = llm.LLMCompletion(self.testOneShot2,queue)
+        self.assertEqual(self.testOneShotAnswer, answer)
+        self.assertEqual(len(queue),5)  # role + 2 messages + 2 answers
         queue.clear()
-        # new conversation not appended to queue
-        answer = llm.LLMCompletion(self.testMessage,queue,False)
+
+        # loss of memory test
+        llm.LLMCompletion(self.testOneShot1)
+        answer = llm.LLMCompletion(self.testOneShot2)
+        self.assertNotEqual(self.testOneShotAnswer, answer)
+
+        # zero-shot conversation
+        answer = llm.LLMCompletion(self.testMessage)
         self.assertEqual(self.testMessage, answer)
         self.assertEqual(len(queue),0)
 
@@ -53,15 +64,26 @@ class TestLLM(unittest.TestCase):
         answer: str = ""
         llm: LLM = LLM(Models.CLAUDE)
         llm.DefineRole(self.testRole)
-        # new conversation appended to queue
-        answer = llm.LLMCompletion(self.testMessage,queue,True)
-        self.assertEqual(self.testMessage, answer)
-        self.assertEqual(len(queue),2)  # role message not put in queue
+
+        # one-shot conversation with queue
+        llm.LLMCompletion(self.testOneShot1,queue)
+        answer = llm.LLMCompletion(self.testOneShot2,queue)
+        self.assertEqual(self.testOneShotAnswer, answer)
+        self.assertEqual(len(queue),4)  # role message not put in queue
         queue.clear()
-        # new conversation not appended to queue
-        answer = llm.LLMCompletion(self.testMessage,queue,False)
+
+        # loss of memory test
+        llm.LLMCompletion(self.testOneShot1)
+        answer = llm.LLMCompletion(self.testOneShot2)
+        self.assertNotEqual(self.testOneShotAnswer, answer)
+
+        # zero-shot conversation
+        answer = llm.LLMCompletion(self.testMessage)
         self.assertEqual(self.testMessage, answer)
         self.assertEqual(len(queue),0)
 
 if __name__ == '__main__':
-    unittest.main(verbosity=0)
+    print("Running unit tests for LLMChat.py")
+    print("This version tests GPT and CLAUDE, but not yet GEMINI")
+    print("If all the tests succeed, you will see a message saying 'OK'")
+    unittest.main(verbosity=1)
